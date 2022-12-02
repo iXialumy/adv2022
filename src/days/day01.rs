de9bff7589380524e850f1188e69a1a2711f8aff
Jonas Paus <pausjonas@gmail.com>
2022-12-02 15:15:43 +0100

    Add Day 1 Solutions

---
+++ src/days/day01.rs
@@ -0,0 +1,33 @@
+use itertools::Itertools;
+
+pub fn day01() {
+    let input = include_str!("../../input/01.txt");
+    day01a(input);
+    day01b(input);
+}
+
+fn day01a(input: &str) {
+    let max: u32 = input.split("\n\n")
+        .map(to_calories)
+        .map(|x| x.iter().sum())
+        .max().unwrap();
+
+    println!("Day01a: {}", max);
+}
+fn day01b(input: &str) {
+    let sum: u32 = input.split("\n\n")
+        .map(to_calories)
+        .map(|x| x.iter().sum::<u32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum();
+
+    println!("Day01b: {}", sum);
+}
+
+fn to_calories(lines: &str) -> Vec<u32> {
+    lines.lines()
+        .filter_map(|x| x.parse().ok())
+        .collect()
+}
